7bcb6f23ee6d8677919fde19d028f3ca3f39a2f7
Markfu1 <13684956108@163.com>
2022-08-09 22:16:26 +0800

    test thead pool

--- src/testone/helloWorld.java
+++ src/testone/helloWorld.java
@@ -1,24 +1,56 @@
 package testone;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class helloWorld {
-
-    public static void main(String[] args) {
-        /*System.out.println("第一种放在常量池中，第二种为引用");
-        int a = 6;
-        int b = 6;
-        System.out.println(a==b);
-
-        String aa = new String("aabb");
-        String bb = new String("aabb");
-        System.out.println(aa == bb);
-
-        final int ME = 0;*/
-        /*volatile int i = 5;*/
-        for (int i = 0; i < 20; i ++){
-            System.out.println(new BigDecimal(Long.MAX_VALUE));
-        }
+    /**
+     * 初始化未来三天的销量
+     */
+    public static Map<Date, Double> initSalesPlanMap() {
+        Map<Date, Double> salesPlanMap = new HashMap<>();
+        long now = System.currentTimeMillis() / 1000l;
+        long daySecond = 60 * 60 * 24;
+        long dayTime = now - (now + 8 * 3600) % daySecond;
+//今天的销量计划-10
+        salesPlanMap.put(initDateByDay(0), 10.00);
+//明天的销量计划-20
+        salesPlanMap.put(initDateByDay(1), 20.00);
+//后台的销量计划-30
+        salesPlanMap.put(initDateByDay(2), 30.00);
+        return salesPlanMap;
     }
-
+    /**
+     * 根据offset获取对应天的数据
+     */
+    public static double getSalesPlan(int offset) {
+        Map<Date, Double> dateDoubleMap = initSalesPlanMap();
+        Date date = initDateByDay(offset);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        date = calendar.getTime();
+        Double result = dateDoubleMap.get(date);
+        return result;
+    }
+    /**
+     * 获得当天零时零分零秒
+     *
+     * @return
+     */
+    public static Date initDateByDay(int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.add(Calendar.DAY_OF_YEAR, day);
+        return calendar.getTime();
+    }
+    public static void main(String[] args) {
+//输出明天的销量计划,预期结果=20
+        System.out.println(getSalesPlan(0));
+    }
 }
